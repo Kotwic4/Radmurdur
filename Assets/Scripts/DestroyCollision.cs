b9b081a6960a55c908d1a8e8de2744505e79e609
Radomir Krawczykiewicz <kotwic4@gmail.com>
2019-03-17 01:44:40 +0100

    Simple Logic

---
+++ Assets/Scripts/DestroyCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCollision : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Generation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour {
+
+    public Object prefab;
+
+    public float generationRate;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Generate", 0, generationRate);
+	}
+
+    void Generate()
+    {
+        Rigidbody parent = GetComponent<Rigidbody>();
+        Instantiate(prefab, parent.position, parent.rotation);
+    }
+}
