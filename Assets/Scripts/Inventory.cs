35a95150b416b0fa6b69ff0aa3e865cf495e20e6
Dariusz Szymczyk <edarek@op.pl>
2019-05-06 14:09:01 +0200

    Inventory base created.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Object slotPrefab;
+    public Object[] prefabs;
+    public int[] amounts;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Rigidbody parent = GetComponent<Rigidbody>();
+
+        for (int i = 0; i < prefabs.Length; i++) {
+            var obj = (GameObject) Instantiate(slotPrefab, parent.position, parent.rotation);
+            obj.transform.parent = gameObject.transform;
+            obj.GetComponent<Rigidbody>().isKinematic = true;
+            obj.GetComponent<Slot>().prefab = prefabs[i];
+        }
+    }
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public Object prefab;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Rigidbody parent = GetComponent<Rigidbody>();
+		var obj = (GameObject) Instantiate(prefab, parent.position, parent.rotation);
+		obj.transform.parent = gameObject.transform;
+        obj.transform.localScale = new Vector3(10, 10, 10);
+		obj.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
