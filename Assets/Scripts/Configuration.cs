c6e27db3f93f400c140efda647088005ebe6f70f
Dariusz Szymczyk <edarek@op.pl>
2019-05-19 10:16:11 +0200

    Generator configuration popup created

---
+++ Assets/Scripts/Configuration.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Configuration : MonoBehaviour
+{
+    public GameObject generator;
+    private InputField amountInput;
+    private InputField rateInput;
+    private Button saveButton;
+    private Button cancelButton;
+
+    void Start() {
+        amountInput = transform.Find("Amount").GetComponent<InputField>();
+        rateInput = transform.Find("Rate").GetComponent<InputField>();
+        saveButton = transform.Find("Save").GetComponent<Button>();
+        cancelButton = transform.Find("Cancel").GetComponent<Button>();
+
+        saveButton.onClick.AddListener(OnSave);
+        cancelButton.onClick.AddListener(OnCancel);
+
+        amountInput.text = generator.GetComponent<Generation>().prefabsToGenerate.ToString();
+        rateInput.text = generator.GetComponent<Generation>().generationRate.ToString();
+    }
+
+    void OnSave() {
+        generator.GetComponent<Generation>().prefabsToGenerate = int.Parse(amountInput.text.ToString());
+        generator.GetComponent<Generation>().generationRate = int.Parse(amountInput.text.ToString());
+
+        OnCancel();
+    }
+
+    void OnCancel() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/OpenConfiguration.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenConfiguration : MonoBehaviour
+{
+    public bool enabled;
+    public Object configuration;
+    public Canvas canvas;
+    private bool hover = false;
+    private bool shown = false;
+    private GameObject configurationGameObject;
+
+    void Update() {
+        if (enabled && hover && Input.GetMouseButtonDown(1) && configurationGameObject == null) {
+            Rigidbody parent = GetComponent<Rigidbody>();
+            configurationGameObject = (GameObject) Instantiate(configuration, parent.position, parent.rotation);
+            configurationGameObject.GetComponent<Configuration>().generator = gameObject;
+
+            configurationGameObject.transform.SetParent(canvas.transform, false);
+            configurationGameObject.transform.position = gameObject.transform.position;
+        }
+    }
+
+    void OnMouseOver()
+    {
+        hover = true; 
+    }
+
+     void OnMouseExit()
+    {
+        hover = false;
+    }
+}
