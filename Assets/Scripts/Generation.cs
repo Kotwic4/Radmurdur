68a34d347884814be9b74d67ef3b106d0ab8b8e2
Marek Kaput <marek.kaput@outlook.com>
2019-05-25 17:26:02 +0200

    add sensible defaults to prefabs & generation script

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -4,13 +4,13 @@ public class Generation : MonoBehaviour
 {
     public Object prefab;
 
-    public int prefabsToGenerate;
+    public int prefabsToGenerate = 1;
 
-    public float generationRate;
+    public float generationRate = .8f;
 
-    public Vector3 initialSpeed;
+    public Vector3 initialSpeed = new Vector3(0f, 0f, 0f);
 
-    public bool useGravity;
+    public bool useGravity = true;
 
     // Use this for initialization
     private void Start()
@@ -18,37 +18,20 @@ public class Generation : MonoBehaviour
         Invoke("Generate", 0);
     }
 
-    public void GenerateInstantiate(Vector3 initialSpeed, bool useGravity)
-    {
-        Rigidbody parent = GetComponent<Rigidbody>();
-        GameObject obj = (GameObject) Instantiate(prefab, parent.position, parent.rotation);
-        obj.GetComponent<Rigidbody>().velocity = initialSpeed;
-        obj.GetComponent<Rigidbody>().useGravity = useGravity;
-        obj.transform.localScale = new Vector3(10, 10, 10);
-    }
-
-    public void GenerateInstantiate(Vector3 initialSpeed)
-    {
-        GenerateInstantiate(initialSpeed, useGravity);
-    }
-
-    public void GenerateInstantiate(bool useGravity)
-    {
-        GenerateInstantiate(initialSpeed, useGravity);
-    }
-
-    public void GenerateInstantiate()
-    {
-        GenerateInstantiate(initialSpeed, useGravity);
-    }
-
     private void Generate()
     {
-        if (prefabsToGenerate != 0)
-        {
-            GenerateInstantiate();
-            prefabsToGenerate = Mathf.Max(prefabsToGenerate, 0) - 1;
-            Invoke("Generate", generationRate);
-        }
+        if (prefabsToGenerate <= 0) return;
+        
+        GenerateInstantiate();
+        prefabsToGenerate -= 1;
+        Invoke("Generate", generationRate);
+    }
+
+    private void GenerateInstantiate()
+    {
+        var parent = GetComponent<Rigidbody>();
+        var obj = (GameObject) Instantiate(prefab, parent.position, parent.rotation);
+        obj.GetComponent<Rigidbody>().velocity = initialSpeed;
+        obj.GetComponent<Rigidbody>().useGravity = useGravity;
     }
 }
